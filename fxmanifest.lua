34d1c8773a2d463b1a152fe3f7cec23c1193f25a
RexShack <rexshackuk@gmail.com>
2022-10-09 09:09:19 +0100

    fix notifications

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,13 +2,13 @@ fx_version 'cerulean'
 game 'rdr3'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
-description 'qr-HUD'
+description 'QR-HUD'
 version '2.0.0'
 
 shared_scripts {
 	'@qr-core/shared/locale.lua',
 	'locales/en.lua',
-    	'config.lua'
+	'config.lua'
 }
 
 client_script 'client/main.lua'
