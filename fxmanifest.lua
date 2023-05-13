9262eba127815fefbb320f9dd6c0b02bb7c11bd8
xThrasherrr <101474430+xThrasherrr@users.noreply.github.com>
2023-05-13 14:01:22 -0600

    chore(manifest): add ox_lib

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,10 +2,13 @@ fx_version 'cerulean'
 game 'rdr3'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
+lua54 'yes'
+
 description 'QR-HUD'
 version '2.0.0'
 
 shared_scripts {
+	'@ox_lib/init.lua',
 	'@qr-core/shared/locale.lua',
 	'locales/en.lua',
 	'config.lua'
